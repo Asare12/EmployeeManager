83bea16125a77f1d0f505d5adce39ffe90c9f6e0
davidasare <gohan.david11@gmail.com>
2021-04-06 17:33:46 +0100

    feat: Adding employee controller class

---
+++ src/main/java/com/davidasare/employeemanager/EmployeeResource.java
@@ -0,0 +1,50 @@
+package com.davidasare.employeemanager;
+
+import com.davidasare.employeemanager.model.Employee;
+import com.davidasare.employeemanager.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable("id") Long id){
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee updatedEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> removeEmployee(@PathVariable("id") Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+}
--- src/main/java/com/davidasare/employeemanager/service/EmployeeService.java
+++ src/main/java/com/davidasare/employeemanager/service/EmployeeService.java
@@ -5,11 +5,13 @@ import com.davidasare.employeemanager.model.Employee;
 import com.davidasare.employeemanager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EmployeeService {
 
     @Autowired
